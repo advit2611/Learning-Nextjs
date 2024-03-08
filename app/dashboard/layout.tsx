8c36709957371908940e3fd70992464007673039
advit2611 <advit214@gmail.com>
2024-03-07 19:26:28 -0500

    Fixed for Readability

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import Link from "next/link";
@@ -8,7 +8,7 @@ interface DashBoardLayoutProps {
 }
 
 const DashBoardLayouts: React.FC<DashBoardLayoutProps> = (props) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
@@ -24,25 +24,37 @@ const DashBoardLayouts: React.FC<DashBoardLayoutProps> = (props) => {
         </ul>
       </nav>
       <div className={"flex"}>
-      <button className="bg-slate-700 py-2 absolute text-cyan-500" onClick={toggleOpen}>{!isOpen ? (<div className=" h-screen bg-slate-700 ">M<br></br>e<br></br>n<br></br>u</div>): 'Menu'}</button>
-        {isOpen && <div className=" h-screen bg-slate-700 w-[20vw] text-slate-300 py-10">
-          <ul className="flex flex-col gap-2">
-            <li>
-              <Link href={"/dashboard"}>Dashboard</Link>
-            </li>
-            <li>
-              <Link href={"/dashboard/todos"}>Todos</Link>
-            </li>
-            <li>
-              <Link href={"/users"}>Users</Link>
-            </li>
-            <li>
-              <Link href={"/dashboard/profile"}>Profile</Link>
-            </li>
-          </ul>
-        </div>}
+        <button
+          className="bg-slate-700 py-2 absolute text-cyan-500"
+          onClick={toggleOpen}
+        >
+          {!isOpen ? (
+            <div className=" h-screen bg-slate-700 ">
+              M<br></br>e<br></br>n<br></br>u
+            </div>
+          ) : (
+            "Menu"
+          )}
+        </button>
+        {isOpen && (
+          <div className=" h-screen bg-slate-700 w-[20vw] text-slate-300 py-10">
+            <ul className="flex flex-col gap-2">
+              <li>
+                <Link href={"/dashboard"}>Dashboard</Link>
+              </li>
+              <li>
+                <Link href={"/dashboard/todos"}>Todos</Link>
+              </li>
+              <li>
+                <Link href={"/users"}>Users</Link>
+              </li>
+              <li>
+                <Link href={"/dashboard/profile"}>Profile</Link>
+              </li>
+            </ul>
+          </div>
+        )}
         <div className=" px-6 max-h-screen overflow-auto">{props.children}</div>
-        
       </div>
     </div>
   );
